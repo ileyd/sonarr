dd5e2776bdd713790578e1cb60a37266bd7fe14e
Jonathan Gilbert <jgilbert@atlassian.com>
2016-12-29 16:16:11 +1100

    added omitempty to all json fields

--- sonarr_types.go
+++ sonarr_types.go
@@ -3,68 +3,68 @@ package go_sonarr
 import "time"
 
 type SeriesPostResponse struct {
-	Title string `json:"title"`
-	SeasonCount int `json:"seasonCount"`
-	EpisodeCount int `json:"episodeCount"`
-	EpisodeFileCount int `json:"episodeFileCount"`
-	Status string `json:"status"`
-	Overview string `json:"overview"`
-	NextAiring time.Time `json:"nextAiring"`
-	Network string `json:"network"`
-	AirTime string `json:"airTime"`
+	Title string `json:"title,omitempty"`
+	SeasonCount int `json:"seasonCount,omitempty"`
+	EpisodeCount int `json:"episodeCount,omitempty"`
+	EpisodeFileCount int `json:"episodeFileCount,omitempty"`
+	Status string `json:"status,omitempty"`
+	Overview string `json:"overview,omitempty"`
+	NextAiring time.Time `json:"nextAiring,omitempty"`
+	Network string `json:"network,omitempty"`
+	AirTime string `json:"airTime,omitempty"`
 	Images []struct {
-		CoverType string `json:"coverType"`
-		URL string `json:"url"`
-	} `json:"images"`
+		CoverType string `json:"coverType,omitempty"`
+		URL string `json:"url,omitempty"`
+	} `json:"images,omitempty"`
 	Seasons []struct {
-		SeasonNumber int `json:"seasonNumber"`
-		Monitored bool `json:"monitored"`
-	} `json:"seasons"`
-	Year int `json:"year"`
-	Path string `json:"path"`
-	QualityProfileID int `json:"qualityProfileId"`
-	SeasonFolder bool `json:"seasonFolder"`
-	Monitored bool `json:"monitored"`
-	UseSceneNumbering bool `json:"useSceneNumbering"`
-	Runtime int `json:"runtime"`
-	TvdbID int `json:"tvdbId"`
-	TvRageID int `json:"tvRageId"`
-	FirstAired time.Time `json:"firstAired"`
-	LastInfoSync time.Time `json:"lastInfoSync"`
-	SeriesType string `json:"seriesType"`
-	CleanTitle string `json:"cleanTitle"`
-	ImdbID string `json:"imdbId"`
-	TitleSlug string `json:"titleSlug"`
-	ID int `json:"id"`
+		SeasonNumber int `json:"seasonNumber,omitempty"`
+		Monitored bool `json:"monitored,omitempty"`
+	} `json:"seasons,omitempty"`
+	Year int `json:"year,omitempty"`
+	Path string `json:"path,omitempty"`
+	QualityProfileID int `json:"qualityProfileId,omitempty"`
+	SeasonFolder bool `json:"seasonFolder,omitempty"`
+	Monitored bool `json:"monitored,omitempty"`
+	UseSceneNumbering bool `json:"useSceneNumbering,omitempty"`
+	Runtime int `json:"runtime,omitempty"`
+	TvdbID int `json:"tvdbId,omitempty"`
+	TvRageID int `json:"tvRageId,omitempty"`
+	FirstAired time.Time `json:"firstAired,omitempty"`
+	LastInfoSync time.Time `json:"lastInfoSync,omitempty"`
+	SeriesType string `json:"seriesType,omitempty"`
+	CleanTitle string `json:"cleanTitle,omitempty"`
+	ImdbID string `json:"imdbId,omitempty"`
+	TitleSlug string `json:"titleSlug,omitempty"`
+	ID int `json:"id,omitempty"`
 }
 
 type SeriesLookupGetResponse []struct {
-	Title string `json:"title"`
-	SeasonCount int `json:"seasonCount"`
-	EpisodeCount int `json:"episodeCount"`
-	EpisodeFileCount int `json:"episodeFileCount"`
-	Status string `json:"status"`
-	Overview string `json:"overview"`
-	Network string `json:"network"`
+	Title string `json:"title,omitempty"`
+	SeasonCount int `json:"seasonCount,omitempty"`
+	EpisodeCount int `json:"episodeCount,omitempty"`
+	EpisodeFileCount int `json:"episodeFileCount,omitempty"`
+	Status string `json:"status,omitempty"`
+	Overview string `json:"overview,omitempty"`
+	Network string `json:"network,omitempty"`
 	Images []struct {
-		CoverType string `json:"coverType"`
-		URL string `json:"url"`
-	} `json:"images"`
-	RemotePoster string `json:"remotePoster"`
+		CoverType string `json:"coverType,omitempty"`
+		URL string `json:"url,omitempty"`
+	} `json:"images,omitempty"`
+	RemotePoster string `json:"remotePoster,omitempty"`
 	Seasons []struct {
-		SeasonNumber int `json:"seasonNumber"`
-		Monitored bool `json:"monitored"`
-	} `json:"seasons"`
-	Year int `json:"year"`
-	QualityProfileID int `json:"qualityProfileId"`
-	SeasonFolder bool `json:"seasonFolder"`
-	Monitored bool `json:"monitored"`
-	UseSceneNumbering bool `json:"useSceneNumbering"`
-	Runtime int `json:"runtime"`
-	TvdbID int `json:"tvdbId"`
-	TvRageID int `json:"tvRageId"`
-	SeriesType string `json:"seriesType"`
-	CleanTitle string `json:"cleanTitle"`
-	ImdbID string `json:"imdbId"`
-	TitleSlug string `json:"titleSlug"`
+		SeasonNumber int `json:"seasonNumber,omitempty"`
+		Monitored bool `json:"monitored,omitempty"`
+	} `json:"seasons,omitempty"`
+	Year int `json:"year,omitempty"`
+	QualityProfileID int `json:"qualityProfileId,omitempty"`
+	SeasonFolder bool `json:"seasonFolder,omitempty"`
+	Monitored bool `json:"monitored,omitempty"`
+	UseSceneNumbering bool `json:"useSceneNumbering,omitempty"`
+	Runtime int `json:"runtime,omitempty"`
+	TvdbID int `json:"tvdbId,omitempty"`
+	TvRageID int `json:"tvRageId,omitempty"`
+	SeriesType string `json:"seriesType,omitempty"`
+	CleanTitle string `json:"cleanTitle,omitempty"`
+	ImdbID string `json:"imdbId,omitempty"`
+	TitleSlug string `json:"titleSlug,omitempty"`
 }
