591d1a8cf360b8b44ae36a825b289eca04039b76
Jonathan Gilbert <jgilbert@atlassian.com>
2016-12-29 15:30:28 +1100

    fixed nil pointer

--- sonarr_calls.go
+++ sonarr_calls.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func (sc *SonarrClient) SeriesLookup(term string) (SeriesLookupGetResponse, error) {
 
-	var rv *SeriesLookupGetResponse
+	rv := &SeriesLookupGetResponse{}
 
 	err := sc.DoRequest("GET", "series/lookup", map[string]string{"term":term}, nil, rv)
 
