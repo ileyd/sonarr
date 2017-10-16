cb48a91897580c377d484b9dd8aeeefcc1a665e5
Luna <luna@stor.lol>
2017-10-16 23:06:36 +1000

    corrected seriesId type to be int

--- command.go
+++ command.go
@@ -3,7 +3,7 @@ package sonarr
 import "github.com/juju/errors"
 
 func (sc *SonarrClient) RescanSeries(seriesID int) (err error) {
-	err = sc.DoRequest("POST", "command", nil, map[string]string{"name": "RescanSeries", "seriesId": string(seriesID)}, nil)
+	err = sc.DoRequest("POST", "command", nil, map[string]interface{}{"name": "RescanSeries", "seriesId": seriesID}, nil)
 
 	if err != nil {
 		return errors.Annotate(err, "Failed to request rescanning of series with ID "+string(seriesID))
